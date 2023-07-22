347159323b3d3955b246e70f292c5cae2c25d016
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-07-22 15:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if (sampleObject.red==key ||sampleObject.green==key ||sampleObject.white==key ||) {
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 // Do not change the code below
